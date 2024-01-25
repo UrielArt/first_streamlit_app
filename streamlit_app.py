11afd5cf4258b1d056e2a69d5279dc9111ccfd53
UrielArt <157658135+UrielArt@users.noreply.github.com>
2024-01-24 18:35:49 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,9 @@
 import streamlit
 
-streamlit.title('My Parents New Healthy Diner')
+streamlit.title('My Mom's New Healthy Diner')
  
-streamlit.header('🥣 Breakfast Menu')
-streamlit.text('🥗 Omega 3 Blueberry Oatmeal')
-streamlit.text('🐔 Kale, Spinach & Rocket Smooothie') 
-streamlit.text('🥑🍞 Hard-Boiled Free-Rage Egg')
+streamlit.header('Breakfast Menu')
+streamlit.text('🥣 Omega 3 Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smooothie') 
+streamlit.text('🐔 Hard-Boiled Free-Rage Egg')
+streamlit.text('🥑🍞 Avocado toast')
